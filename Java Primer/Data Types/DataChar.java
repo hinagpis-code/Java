bd1792bd9d89b0af96feaa545e4133e486ba76cf
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-02-06 18:51:59 +0800

    Character Data Type

---
+++ Java Primer/Data Types/DataChar.java
@@ -0,0 +1,18 @@
+public class DataChar{
+  
+ public static void main(String[] args) {
+   
+   /**
+   Char short for Character is a single-character text.
+   - use single-quote if you are using character data type
+   */
+   
+   char first_name = 'H';
+   char last_name = 'C';
+   
+   System.out.print(first_name); //output: H
+   System.out.print(last_name); //output: C
+   
+ }
+  
+}
