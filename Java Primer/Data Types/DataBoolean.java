b85ab280444491f3b047ad20b33cb8377fed56a3
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-02-06 18:54:53 +0800

    Boolean Data Type

---
+++ Java Primer/Data Types/DataBoolean.java
@@ -0,0 +1,21 @@
+public class DataBoolean {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Boolean data has only 2 values
+		 * True or False only
+		 */
+		
+		boolean x = true;
+		boolean y = false;
+		
+		System.out.println(x); //output : True
+		System.out.println(y); //output : False
+
+    int x = 10, y = 11;
+    
+    System.out.println(y>x); //output: True
+	}
+
+}
