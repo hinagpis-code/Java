1a9d4c356857d202dd9820ec5c212aaea366f5ed
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-02-07 16:42:33 +0800

    Formatted Print

---
+++ Java Primer/Print Statement/PrintFormatted.java
@@ -0,0 +1,14 @@
+public class PrintFormatted{
+  
+ public static void main(String[] args){
+   
+   String name = "Hinagpis Code";
+   int age = 20;
+   
+   System.out.printf("My age is %d", age);
+   //this is same as
+   System.out.format("My name is %s", name);
+   
+   
+    }
+}
