75a7662d5a5371830e55b5faf8d51ec4bf3d3e1f
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-02-07 16:12:47 +0800

    One Line Print

--- Java Primer/Print Statement/Print.java
+++ Java Primer/Print Statement/Print.java
@@ -1,1 +1,13 @@
 
+public class Print{
+ 
+  public static void main(String[] args){
+    /*
+    Print statement allows you to print in a single line without
+    moving to the next line.
+    */
+   System.out.print("Hello World!"); 
+    
+  }
+  
+}
