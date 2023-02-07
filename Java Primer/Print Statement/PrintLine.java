e1486028e7511d0ba4336ecae4a7a45f23eb67cf
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-02-07 16:15:31 +0800

    New Line Print

---
+++ Java Primer/Print Statement/PrintLine.java
@@ -0,0 +1,13 @@
+public class PrintLine{
+  
+ public static void main(String[] args){
+   
+   /*
+   Print line allows you to print the value and create a new line
+   */
+   
+  System.out.println("Hello World!");
+   
+   
+ }
+}
