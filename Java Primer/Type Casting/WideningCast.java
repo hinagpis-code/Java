e6a942e0f29c821fdb6203d8c2b3ea3ff938dbad
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-02-08 08:35:12 +0800

    Widening Casting

---
+++ Java Primer/Type Casting/WideningCast.java
@@ -0,0 +1,20 @@
+/*
+  Widening Casting allows you to
+  convert the type from smaller to larger type size.
+  
+  From the word it self Wide, small to larger
+*/
+
+public class WideningCast{
+  
+ public static void main(Stirng[] args){
+   
+  short average = 89;
+   
+  int myAverage = average; //Automatic change data type from short > int
+   
+   System.out.print(myAverage);
+  
+ }
+  
+}
