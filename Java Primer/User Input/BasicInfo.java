871c625f1c137fe2c008018002485efb05a7f997
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-02-10 09:18:50 +0800

    Input Basic Information

--- Java Primer/User Input/BasicInfo.java
+++ Java Primer/User Input/BasicInfo.java
@@ -14,14 +14,14 @@ public class BasicInfo{
    System.out.print("Enter your First Name: ");
    String fname = input.nextLine();
    
-   System.out.print("Enter your Middle Name: ");
-   String mname = input.nextLine();
+   System.out.print("Enter your Middle Initial: ");
+   char mi = input.nextLine().charAt(0);
    
    System.out.print("Enter your address: ");
    String address = input.nextLine();
    
-   //print the inputted data
-   System.out.println("\nMy name is " + lname + ", " + fname + " " + mname +".");
+   
+   System.out.println("\nMy name is " + lname + ", " + fname + " " + mi +".");
    System.out.println("I live in " + address);
     
   }
