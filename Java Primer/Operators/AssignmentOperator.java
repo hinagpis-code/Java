c77020dbf3756732379ab11e8f2b53c8346818bb
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-02-11 08:18:04 +0800

    Assignment Operator

---
+++ Java Primer/Operators/AssignmentOperator.java
@@ -0,0 +1,27 @@
+public class AssignmentOperator{
+  
+ public static void main(String [] args){
+   
+  //Assignment operators are used to assign values to variables.
+   
+  /*In the example below, we use the assignment operator (=)
+  to assign the value 10 to a variable called x:
+  */
+   
+   int x = 10;
+   
+   x += 5; //Expanded Form: x = x + 5 or 10 + 5
+   System.out.println(x) // Output: 15
+     
+   x -= 5; //Expanded Form: x = x - 5 or 10 - 5
+   System.out.println(x); //Output: 5
+   
+   x *= 10; //Expanded Form: x = x * 10 or 10 * 10
+   System.out.println(x); // Output: 100
+     
+   x /= 2; //EXpanded FOrm: x = x / 2 or 10 / 2
+   System.out.println(x); //Output: 5
+   
+ }
+  
+}
