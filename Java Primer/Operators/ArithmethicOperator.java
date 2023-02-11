a9d23b045ddfb43b5b8508784c328713314afaad
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-02-11 08:01:33 +0800

    Arithmethic Operator

---
+++ Java Primer/Operators/ArithmethicOperator.java
@@ -0,0 +1,37 @@
+public class ArithmethicOperator{
+  
+ public static void main(String [] args){
+  
+   
+   int x = 10, y = 11;
+   
+   //Addition Operator (+)  = adds together the two value.
+   System.out.println(x+y);
+   
+   //Subtraction Operator (+)  = Subtract one from another.
+   System.out.println(x-y);
+   
+   //Multiplication Operator (*) = Multiplies the two values.
+   System.out.println(x*y);
+   
+   //Division Operator (/) = Divides one from another.
+    System.out.println(x/y);
+   
+   //Modulus Operator (%) = Return the remainder of division
+    System.out.println(x%y);
+   
+   //Increment Operator (++) = Increase the value of variable by 1.
+    System.out.println(x++);
+    System.out.println(y++);
+   
+   //Decrement Operator (--) = Decrease the value of variable by 1.
+     System.out.println(x--);
+    System.out.println(y--);
+   
+   
+   
+   
+   
+ }
+  
+}
