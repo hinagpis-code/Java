e19aaccf38bd0fae41c40429f8b1d7404a175f3e
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-02-11 09:52:46 +0800

    Create StringLength.java

---
+++ Java Primer/Data Types/StringLength.java
@@ -0,0 +1,15 @@
+public class StringLength{
+  
+ public static void main(String[] args){
+  
+   //length() method counts the String value of your variable
+   
+   String name = "Hinagpis Code";
+   
+   System.out.print(name.length()); //Output: 13
+   
+   
+   
+ }
+  
+}
