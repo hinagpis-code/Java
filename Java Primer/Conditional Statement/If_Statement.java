deff7859e92de64c8f7acbb81f67b217721ad62f
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-02-17 08:53:19 +0800

    Create If_Statement.java

---
+++ Java Primer/Conditional Statement/If_Statement.java
@@ -0,0 +1,26 @@
+public class If_Statement{
+  
+  public static void main(Sting[] args){
+   
+    /*
+		 * 		if (condition is true) {
+		 * 			print
+		 * 		}
+		 * 
+		 * We will use the logic 
+		 * 
+		 */
+		
+		int x = 10, y = 20;
+		
+		if(x < y) { //this is true because 10 is less than 20
+			
+			System.out.println(x + " is less than " + y); 	//output : 10 is less than 20
+			
+		}
+    
+    //If condition is false there's nothing to print
+    
+  }
+  
+}
