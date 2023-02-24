344ea509b21676d2f7a27c9b7da17bb695a67d72
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-02-24 08:17:40 +0800

    Create StringEquals.java

---
+++ Java Primer/String/StringEquals.java
@@ -0,0 +1,10 @@
+public class StringEquals {
+    public static void main(String args[]) {
+      
+      String fname = "Juan", lname = "Dela Cruz";
+
+      System.out.println(fname.equals(lname));
+      /**the value of fname is not equal to lname
+       * that's why it return false*/
+    }
+}
