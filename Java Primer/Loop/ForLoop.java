8def6ecb3e248bd98488eff1318c5c78df5829aa
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-02-28 14:40:14 +0800

    Create ForLoop.java

---
+++ Java Primer/Loop/ForLoop.java
@@ -0,0 +1,13 @@
+public class ForLoop {
+
+	public static void main(String[] args) {
+    
+		//it will print "Hello World!" five times
+    
+		for(int i = 1; i<=5;i++) {
+      
+			System.out.println("Hello World!");
+      
+		}
+	}
+}
