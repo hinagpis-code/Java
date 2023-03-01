9608e6a70f2f2a892c70c5f8588d99551d4b7ac5
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-03-01 13:29:03 +0800

    Create ForEach.java

---
+++ Java Primer/Loop/ForEach.java
@@ -0,0 +1,23 @@
+public class ForEach {
+    public static void main(String args[]) {
+         
+         String [] names = {"Hinagpis", "Code", "BSIT"};
+         
+			
+			
+         for(String val : names ) {
+             System.out.println(val);
+         }
+			
+			/* 
+					for(Data type for container : Array name) {
+							System.out.println(container name);	
+						}
+			
+			For each loop will print all the element 
+			inside the array.
+			If you are confused in identifying the size of 
+			array, use for each loop.
+			*/
+    }
+}
