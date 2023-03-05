d4eaae4fdf5a5d8487b769edc1d40b4c66910750
Certain <124571485+hinagpis-code@users.noreply.github.com>
2023-03-05 20:50:44 +0800

    Create SwitchCase.java

---
+++ Java Primer/Conditional Statement/SwitchCase.java
@@ -0,0 +1,26 @@
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+		
+		
+		int number = 2;
+		
+		switch (number) {
+		
+		case 1 : 
+			System.out.println("This is number " + number);
+			break;
+			
+		case 2 :
+			System.out.println("This is number " + number);
+			break;
+			
+		default :	
+			System.out.println("This number is greater than 1 and 2 ");
+			break;	
+		}
+		
+	}
+
+}
